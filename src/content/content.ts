d80a5ac971db5251bd746db095e6f6fc5b16c443
LRa <40637257+LucyRa@users.noreply.github.com>
2024-08-02 22:14:38 +0100

    Playing around

--- src/content/content.ts
+++ src/content/content.ts
@@ -1,19 +1,32 @@
 import { z, defineCollection } from "astro:content";
 
-const membersCollection = defineCollection({
-  type: 'data',
+const componentsCollection = defineCollection({
+  type: 'content',
   schema: z.object({
+    id: z.number(),
+    title: z.string(),
+    tags: z.array(z.string()),
+    slug: z.string(),
+    created_at: z.date()
+  })
+})
+
+const membersCollection = defineCollection({
+  type: 'content',
+  schema: z.object({
+    position: z.number(),
     firstName: z.string(),
     lastName: z.string(),
     role: z.enum(['Developer', 'Designer', 'Project Manager']),
-    image: z.object({
-      src: z.string(),
-      alt: z.string(),
-    }),
+    // image: z.object({
+    //   src: z.string(),
+    //   alt: z.string(),
+    // }),
     blurb: z.string()
   })
 })
 
 export const collections = {
+  'components': componentsCollection,
   'members': membersCollection
 }
---
+++ src/content/members/member-01.md
@@ -0,0 +1,13 @@
+---
+position: 1
+firstname: El
+lastname: Jeffe
+role: Developer
+blurb: lorem ipsum dolor
+---
+
+# El Jeffe
+
+lorem ipsum dolor
+
+Developer.
