90a80c62e20b19da92f4f572990ea0fa2ac26675
LRa <40637257+LucyRa@users.noreply.github.com>
2024-08-04 17:33:14 +0100

    Adding more base componet structures

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -4,3 +4,13 @@ export interface CallToAction {
   external?: boolean;
   className?: string;
 }
+
+export interface Image {
+  src: string;
+  alt?: string | null;
+}
+
+export interface Video {
+  src: string;
+  alt?: string | null;
+}
