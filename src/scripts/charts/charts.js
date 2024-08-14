334a89a3c7189432365a30a974b92e7eef62f61b
LRa <40637257+LucyRa@users.noreply.github.com>
2024-08-14 23:22:46 +0100

    Working on a basic D3 donut chart

--- src/scripts/charts/charts.js
+++ src/scripts/charts/charts.js
@@ -3,7 +3,8 @@ import { initDonut } from "./donut";
 export default function charts () {
   console.log('Charts :)')
 
-  let donutChartEls = document.querySelectorAll(['data-chart-donut'])
+  let donutChartEls = document.querySelectorAll('[data-chart-donut]')
+  console.log(donutChartEls)
   if (donutChartEls) {
     Array.from(donutChartEls, donutChartEl => {
       initDonut(donutChartEl)
--- src/scripts/charts/donut.js
+++ src/scripts/charts/donut.js
@@ -1,21 +1,79 @@
 import * as d3 from 'd3';
 
-const initDonut = (donutChartEl) => {
-  if (donutChartEl) {
-    console.log('Init Donut :)')
-
-    // Declare chart dimensions & margins
-    let width = donutChartEl.parentElement.clientWidth ?? 500
-    let pieDimensions = { h: width, w: width, r: halfWidth }
-    let pieCenter = { x: halfWidth, y: halfWidth }
-
-    let arc = d3.arc()
-      .innerRadius(pieDimensions.r * 0.8)
-      .outerRadius(pieDimensions.r)
-
-  } else {
-    console.warn('Init Donut: No element.');
-    return;
+class Donut {
+  constructor (name, data) {
+    this.name = name || "test"
+    this.data = data || [{
+      name: 'test-1',
+      count: 280
+    },
+    {
+      name: 'test-2',
+      count: 60
+    },
+    {
+      name: 'test-3',
+      count: 20
+    },]
+    this.el = null
+    this.dimensions = { width: 0, radius: 0 }
   }
+
+  // TODO: Turn into a promise...
+  setInstanceElement (name) {
+    if (!name) return console.warn('Donut, instance element: No name');
+
+    let targetEl = document.querySelector(`#donut-chart-${name}`)
+    if (!targetEl) return console.warn('Donut, instance element: No target element');
+
+    this.el = targetEl;
+  }
+
+  setInstanceDimensions (el) {
+    if (!el) return console.warn('Donut, instance dimensions: No target element');
+
+    this.dimensions.width = el.outerWidth
+    this.dimensions.radius = this.dimensions.width / 2;
+  }
+
+
+  buildChart () {
+    const arc = d3.arc()
+      .innerRadius(this.dimensions.radius * 0.8)
+      .outerRadius(this.dimensions.radius)
+      .cornerRadius(6)
+
+    const pie = d3.pie()
+      .padAngle(0.024)
+      .sort(null)
+      .value(d => d.count)
+
+    const colours = d3.scaleOrdinal(
+      this.data.map(d => d.name), d3.quantize(t => d3.interpolateSpectral(t * 0.8 + 0.1), 
+      this.data.length
+    ).reverse())
+
+    const svg = d3.create('svg')
+      .attr('width', this.height)
+      .attr('height', this.height)
+      .attr("viewBox", [-this.height / 2, -this.height / 2, this.height, this.height])
+      .attr("style", "max-width: 100%; height: auto;")
+
+    svg.append('g')
+      .selectAll()
+      .data(pie(this.data))
+
+    console.log(svg)
+  }
+
+  init () {
+    console.log('Donut: init()')
+    this.setInstanceElement(this.name)
+
+    this.buildChart()
+  }
+
+
 }
-export { initDonut };
+
+export { Donut };
